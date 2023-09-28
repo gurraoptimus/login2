396fe22c02842c117e5525a59f3b87ec79d90e20
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-09-29 00:01:31 +0200

    src-link-script

--- src/src.js
+++ src/src.js
@@ -1,3 +1,7 @@
 const wrapper = document.querySelector(".wrapper");
 const regiserlink = document.querySelector(".regiser-link");
 const loginlink = document.querySelector(".login-link");
+
+regiserlink.onclick =() =>{
+    wrapper.
+}
